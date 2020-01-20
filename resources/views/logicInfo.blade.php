b5ee5b5b5921f4197d4b8488ac3a4a3262cf8beb
Davidbyrneit <davidbyrneit@gmail.com>
2020-01-20 00:15:32 +0000

    infor

---
+++ resources/views/logicInfo.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Escape CS</title>
+
+    </head>
+    <body>
+
+    NOT Gate
+ 
+ The NOT gate is also known as an inverter because the output is the exact opposite of the input. It has one input and one output. The two possibilities are written out in the table below. Tables listing all logical possibilities like this are known as truth tables.
+ 
+    </body>
+</html>
