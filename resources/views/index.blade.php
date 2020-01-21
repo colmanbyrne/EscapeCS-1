32683018954b21d005e01ace387f320fed07f593
Davidbyrneit <davidbyrneit@gmail.com>
2020-01-21 22:31:59 +0000

    images

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,10 +5,20 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Escape CS</title>
-
+        <link rel="stylesheet" href="/CSS/style.css">
     </head>
     <body>
+    <div class="grid-container">
+  <div class="grid-item"><a href=" "><img src="img/SVG/Asset 2.svg"> alt=""></div>
+  <div class="grid-item"></div>
+  <div class="grid-item"><img src="img/SVG/Asset 3.svg"  alt=""></div>
+  <div class="grid-item"><img src="img/SVG/Asset 4.svg" alt=""></div>
+  <div class="grid-item"></div>
+  <div class="grid-item"><img src="img/SVG/Asset 5.svg" alt=""></div>
+  <div class="grid-item"><img src="img/SVG/Asset 6.svg" alt=""></div>
+  <div class="grid-item"></div>
+  <div class="grid-item"><img src="img/SVG/Asset 7.svg" alt=""></div>
+</div>
 
-<img src="{{asset('img/Artboard 1.png')}}">
     </body>
 </html>
