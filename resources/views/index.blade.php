6f0e9aede2b71346bfcd239a96d58f535b7c71ad
Davidbyrneit <davidbyrneit@gmail.com>
2020-01-21 22:55:52 +0000

    img 2

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,11 +12,14 @@
   <div class="grid-item"><a href="/logicInfo"><img src="img/SVG/Asset 2.svg" alt=""></div>
   <div class="grid-item"></div>
   <div class="grid-item"><a href="/logicInfo"><img src="img/SVG/Asset 3.svg" alt=""></div>
-  <div class="grid-item"><a href="/logicInfo"><img src="img/SVG/Asset 4.svg" alt=""></div>
-  <div class="grid-item"></div>
+ 
+  
   <div class="grid-item"><a href="/logicInfo"><img src="img/SVG/Asset 5.svg" alt=""></div>
-  <div class="grid-item"><a href="/logicInfo"><img src="img/SVG/Asset 6.svg" alt=""></div>
   <div class="grid-item"></div>
+  <div class="grid-item"></div>
+  <div class="grid-item"><a href="/logicInfo"><img src="img/SVG/Asset 6.svg" alt=""></div>
+  <div class="grid-item"><a href="/logicInfo"><img src="img/SVG/Asset 4.svg" alt=""></div>
+  
   <div class="grid-item"><a href="/logicInfo"><img src="img/SVG/Asset 7.svg" alt=""></div>
 </div>
 
