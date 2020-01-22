4a59e2b7c8238d2d5372f196c31091384f5acf14
Davidbyrneit <davidbyrneit@gmail.com>
2020-01-22 13:27:05 +0000

    nodelogin

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,7 @@
 
 Route::get('/', 'PagesController@index');
 Route::get('/logicInfo', 'PagesController@logicInfo');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
