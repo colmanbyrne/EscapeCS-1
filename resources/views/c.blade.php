65b0015994f2030f21910ba3b550d813008111a2
Davidbyrneit <davidbyrneit@gmail.com>
2020-02-02 18:32:31 +0000

    c puzzle start

--- resources/views/c.blade.php
+++ resources/views/c.blade.php
@@ -1,5 +1,22 @@
 @extends('layouts.app')
+<style>
+.cMemory{
 
+display: grid;
+grid-template-columns:10% 10% 10% 10%;
+grid-template-rows:auto;
+}
+
+.gridData{
+font-size: 150%;
+border: 1px solid black;
+text-align: center;
+padding: 1%;
+}
+
+
+
+</style>
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
@@ -15,12 +32,107 @@
                     @endif
 
                     <div class="cMemory">
-    <div class="gridData">col1</div>
-    <div class="gridData">col2</div>
-    <div class="gridData">col3</div>
-    <div class="gridData">0100</div>
-    <div class="gridData">010a</div>
-    <div class="gridData">010b</div>
+    <div class="gridData">Memory Address</div>
+    <div class="gridData">Memory Content</div>
+    <div class="gridData">Memory Allocation</div>
+    <div class="gridData">Code</div>
+    <div class="gridData">0x07A</div>
+    <div class="gridData">2</div>
+    <div class="gridData">3</div>
+    <div class="gridData">4</div>
+    <div class="gridData">0x07A</div>
+    <div class="gridData">2</div>
+    <div class="gridData">3</div>
+    <div class="gridData">4</div>
+    <div class="gridData">0x07A</div>
+    <div class="gridData">2</div>
+    <div class="gridData">3</div>
+    <div class="gridData">4</div>
+    <div class="gridData">0x07A</div>
+    <div class="gridData">2</div>
+    <div class="gridData">3</div>
+    <div class="gridData">4</div>
+    <div class="gridData">0x07A</div>
+    <div class="gridData">2</div>
+    <div class="gridData">3</div>
+    <div class="gridData">4</div>
+    <div class="gridData">0x07A</div>
+    <div class="gridData">2</div>
+    <div class="gridData">3</div>
+    <div class="gridData">4</div>
+    <div class="gridData">0x07A</div>
+    <div class="gridData">2</div>
+    <div class="gridData">3</div>
+    <div class="gridData">4</div>
+    <div class="gridData">0x07A</div>
+    <div class="gridData">2</div>
+    <div class="gridData">3</div>
+    <div class="gridData">4</div>
+    <div class="gridData">0x07A</div>
+    <div class="gridData">2</div>
+    <div class="gridData">3</div>
+    <div class="gridData">4</div>
+    <div class="gridData">0x07A</div>
+    <div class="gridData">2</div>
+    <div class="gridData">3</div>
+    <div class="gridData">4</div>
+    <div class="gridData">0x07A</div>
+    <div class="gridData">2</div>
+    <div class="gridData">3</div>
+    <div class="gridData">4</div>
+    <div class="gridData">0x07A</div>
+    <div class="gridData">2</div>
+    <div class="gridData">3</div>
+    <div class="gridData">4</div>
+    <div class="gridData">0x06F</div>
+    <div class="gridData">2</div>
+    <div class="gridData">3</div>
+    <div class="gridData">4</div>
+    <div class="gridData">0x06E</div>
+    <div class="gridData">2</div>
+    <div class="gridData">3</div>
+    <div class="gridData">4</div>
+    <div class="gridData">0x06D</div>
+    <div class="gridData">2</div>
+    <div class="gridData">3</div>
+    <div class="gridData">4</div>
+    <div class="gridData">0x06C</div>
+    <div class="gridData">2</div>
+    <div class="gridData">3</div>
+    <div class="gridData">4</div>
+    <div class="gridData">0x06B</div>
+    <div class="gridData">2</div>
+    <div class="gridData">3</div>
+    <div class="gridData">4</div>
+    <div class="gridData">0x06A</div>
+    <div class="gridData">2</div>
+    <div class="gridData">3</div>
+    <div class="gridData">4</div>
+    <div class="gridData">0x069</div>
+    <div class="gridData">2</div>
+    <div class="gridData">3</div>
+    <div class="gridData">4</div>
+    <div class="gridData">0x068</div>
+    <div class="gridData">2</div>
+    <div class="gridData">3</div>
+    <div class="gridData">4</div>
+    <div class="gridData">0x067</div>
+    <div class="gridData">2</div>
+    <div class="gridData">3</div>
+    <div class="gridData">4</div>
+    <div class="gridData">0x066</div>
+    <div class="gridData">2</div>
+    <div class="gridData">3</div>
+    <div class="gridData">4</div>
+    <div class="gridData">0x065</div>
+    <div class="gridData">2</div>
+    <div class="gridData">3</div>
+    <div class="gridData">4</div>
+    <div class="gridData">0x064</div>
+    <div class="gridData">2</div>
+    <div class="gridData">3</div>
+    <div class="gridData">4</div>
+    
     </div>
                 </div>
             </div>
