f1028c27328dcbca7fa7a9030b1db2be1f6283d9
DavidByrneit <davidbyrneit@gmail.com>
2020-09-02 20:03:48 +0100

    h

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -239,8 +239,33 @@ class HomeController extends Controller
         $input =  $_POST["input1"];
         $input2 =  $_POST["input2"];
         $date = Carbon::now();
+
+        $gender =  $_POST["gender"];
+        $age =  $_POST["age"];
+        $schoolLevel =  $_POST["schoolLevel"];
+        $country =  $_POST["country"];
+        $subject =  $_POST["subject"];
+
+        $q1 =  $_POST["q1"];
+        $q2 =  $_POST["q2"];
+        $q3 =  $_POST["q3"];
+        $q4 =  $_POST["q4"];
+        $q5 =  $_POST["q5"];
+        $q6 =  $_POST["q6"];
+        $q7 =  $_POST["q7"];
+        $q8 =  $_POST["q8"];
+        $q9 =  $_POST["q9"];
+        $q10 =  $_POST["q10"];
+        $q11 =  $_POST["q11"];
+        $q12 =  $_POST["q12"];
+        $q13 =  $_POST["q13"];
+        $q14 =  $_POST["q14"];
+        $q15 =  $_POST["q15"];
+        
+
         DB::table('feedback')->insert(
-            ['Email' =>$input2 , 'Date' => $date, 'Comment' => $input]
+            ['Email' =>$input2 , 'Date' => $date, 'Comment' => $input, 'Gender' => $gender, 'Age' => $age, 'SchoolLevel' => $schoolLevel, 'Country' => $country
+            , 'Subject' => $subject, 'q1' => $q1, 'q2' => $q2, 'q3' => $q3, 'q4' => $q4, 'q5' => $q5, 'q6' => $q6, 'q7' => $q7, 'q8' => $q8, 'q9' => $q9, 'q10' => $q10, 'q11' => $q11, 'q12' => $q12, 'q13' => $q13, 'q14' => $q14, 'q15' => $q15]
         );
         return view('home');
 
--- resources/views/final.blade.php
+++ resources/views/final.blade.php
@@ -29,9 +29,89 @@
 Please leave some feedback (I will probably ignore it)
 
                     <?php echo Form::open(array('route' => 'feedback'));
-                       
-                       echo Form::label('input2', 'E-Mail Address');
-                            echo Form::text('input2');
+
+                        echo Form::label('input2', 'E-Mail Address');
+                        echo Form::text('input2');
+                        echo "</br>";
+                        echo Form::label('gender', 'Gender');
+                       echo Form::select('gender', array('M' => 'Male', 'F' => 'Female'));
+                       echo "</br>";
+                       echo Form::label('age', 'Age');
+                            echo Form::number('age');
+                            echo "</br>";
+                            echo Form::label('schoolLevel', 'School Level');
+                            echo Form::text('schoolLevel');
+                            echo "</br>";
+                            echo Form::label('country', 'Country');
+                            echo Form::text('country');
+                            echo "</br>";
+                            echo Form::label('subject', 'Subject');
+                            echo Form::text('subject');
+
+                            echo "</br>";
+                            echo Form::label('q1', 'How many hours do you play video games per week?');
+                            echo Form::number('q1');
+                            echo "</br>";
+                            echo Form::label('q2', 'How many hours do you play roleplay games per week?');
+                            echo Form::number('q2');
+                            echo "</br>";
+                            echo Form::label('q3', 'How many hours do you play boardgames per week?');
+                            echo Form::number('q3');
+
+                            echo "</br>";
+                            echo Form::label('q4', 'I enjoyed while playing the escape room');
+                            echo Form::select('q4', array(1 => 'Strongly Disagree', 2 => 'Disagree', 3 => 'Agree',  4 => 'Strongly Agree'));
+
+                            echo "</br>";
+                            echo Form::label('q5', 'I have found the theme/story interesting');
+                            echo Form::select('q5', array(1 => 'Strongly Disagree', 2 => 'Disagree', 3 => 'Agree',  4 => 'Strongly Agree'));
+
+                            echo "</br>";
+                            echo Form::label('q6', 'The difficulty of the escape room was adequate');
+                            echo Form::select('q6', array(1 => 'Strongly Disagree', 2 => 'Disagree', 3 => 'Agree',  4 => 'Strongly Agree'));
+
+                            echo "</br>";
+                            echo Form::label('q7', 'I am satisfied with the work I did during the escape room');
+                            echo Form::select('q7', array(1 => 'Strongly Disagree', 2 => 'Disagree', 3 => 'Agree',  4 => 'Strongly Agree'));
+
+                            echo "</br>";
+                            echo Form::label('q8', 'We kept track of the given time limit to solve puzzles through team working and communication');
+                            echo Form::select('q8', array(1 => 'Strongly Disagree', 2 => 'Disagree', 3 => 'Agree',  4 => 'Strongly Agree'));
+
+                            echo "</br>";
+                            echo Form::label('q9', 'The escape room was useful for learning new things on the topic');
+                            echo Form::select('q9', array(1 => 'Strongly Disagree', 2 => 'Disagree', 3 => 'Agree',  4 => 'Strongly Agree'));
+
+                            echo "</br>";
+                            echo Form::label('q10', 'The discussion was useful for learning new things on the topic');
+                            echo Form::select('q10', array(1 => 'Strongly Disagree', 2 => 'Disagree', 3 => 'Agree', 4 => 'Strongly Agree'));
+
+                            echo "</br>";
+                            echo Form::label('q10', 'The puzzles and riddles used in this escape room were appealing');
+                            echo Form::select('q10', array(1 => 'Strongly Disagree', 2 => 'Disagree', 3 => 'Agree',  4 => 'Strongly Agree'));
+
+                            echo "</br>";
+                            echo Form::label('q11', 'As a general conclusion, the escape room was a good experience');
+                            echo Form::select('q11', array(1 => 'Strongly Disagree', 2 => 'Disagree', 3 => 'Agree',  4 => 'Strongly Agree'));
+
+                            echo "</br>";
+                            echo Form::label('q12', 'The escape room is a good choice for learning new topics');
+                            echo Form::select('q12', array(1 => 'Strongly Disagree', 2 => 'Disagree', 3 => 'Agree',  4 => 'Strongly Agree'));
+
+                            echo "</br>";
+                            echo Form::label('q13', 'I would suggest other teachers to use ER as well');
+                            echo Form::select('q13', array(1 => 'Strongly Disagree', 2 => 'Disagree', 3 => 'Agree',  4 => 'Strongly Agree'));
+
+                            echo "</br>";
+                            echo Form::label('q14', 'The time invested in class to play the escape room was worthy');
+                            echo Form::select('q14', array(1 => 'Strongly Disagree', 2 => 'Disagree', 3 => 'Agree',  4 => 'Strongly Agree'));
+
+                            echo "</br>";
+                            echo Form::label('q15', 'I would like to participate in more escape rooms');
+                            echo Form::select('q15', array(1 => 'Strongly Disagree', 2 => 'Disagree', 3 => 'Agree',  4 => 'Strongly Agree'));
+
+                           
+                           
                             echo "</br>";
                             echo Form::label('input1', 'Comments');
                             echo Form::textarea('input1');
